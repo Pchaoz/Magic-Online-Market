b45ac7c13f530cf28a0aa0b56d9ecd0d260b0459
Pchaozz <polsh2001.1@gmail.com>
2024-04-02 20:17:00 +0200

    v0.0.1
    - Creados algunos componentes de prueba con cosas basicas en la carpeta "TestComponents"
    
    - Setup base completado, estoy estudiando como hacer el routing y podre ponerme a ello con el projecto

--- magic-online-market/src/App.tsx
+++ magic-online-market/src/App.tsx
@@ -1,11 +1,20 @@
-import ListGroup from "./components/ListGroup";
+import { useState } from "react";
+import Button from "./components/TestComponents/Button";
+import Altert from "./components/TestComponents/Altert";
+
 
 
 function App() {
+  const [showAlert, setAlert] = useState(false);
 
-  return <div>
-    <ListGroup/>
-  </div>
+  return (
+    <div>
+      {showAlert && <Altert onClose={() => setAlert(false)}> Hello <span> world </span> </Altert> }
+      <Button color="dark" onClick={() => setAlert(true)}> Soy un boton </Button>
+    </div>
+  )
+  
+  
 }
 
 export default App;
---
+++ magic-online-market/src/components/TestComponents/Button.tsx
@@ -0,0 +1,14 @@
+interface Props {
+    children: string;
+    color?: 'primary' | 'secondary' | 'danger' | 'dark';
+    onClick: () => void;
+}
+
+const Button = ({children, onClick, color = 'primary'}: Props) => {
+  return (
+
+    <button type="button" className={'btn btn-' + color} onClick={onClick}>{children}</button>
+  )
+}
+
+export default Button
