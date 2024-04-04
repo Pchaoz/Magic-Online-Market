bc63a9f1c5eebec6a07940ff61a6a87756419569
Pchaozz <polsh2001.1@gmail.com>
2024-04-04 16:19:32 +0200

    v0.0.3
    - Handle login
    
    - Movidos archivos para que la organizacion sea mas limpia y correcta

--- magic-online-market/src/components/pages/Login.tsx
+++ magic-online-market/src/components/pages/Login.tsx
@@ -1,14 +1,35 @@
 import { Link } from "react-router-dom"
+import { useState } from "react"
 
-const handleLogin = (event: React.MouseEvent) => {
-  event.preventDefault();
-  console.log(event);
+interface FormData {
+  user : {
+    username : string,
+    password : string
+  }
 }
 
 const Login = () => {
+
+  const [data, setData] = useState<FormData>({
+    user : {
+      username : '',
+      password : ''
+
+    }
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) : void => {
+    setData({
+        user: {
+          ...data.user,
+          [event.target.name] : event.target.value,
+        }
+    })
+  }
+
   return (
     <>
-      <form onSubmit={(event) => handleLogin} className="vh-100">
+      <form className="vh-100">
         <div className="container py-5 h-100">   
             <div className="row d-flex justify-content-center align-items-center h-100">
                 <div className="col-12 col-md-8 col-lg-6 col-xl-5"> 
@@ -20,19 +41,35 @@ const Login = () => {
                                 <p className="text-white-50 mb-5">Porfavor indica tu usario y contraseña!</p>
 
                                 <div className="form-outline form-white mb-4">
-                                    <input type="text" id="typeUserX" className="form-control form-control-lg" />
+                                    <input 
+                                      type="text" 
+                                      name="username" 
+                                      id="typeUserX" 
+                                      className="form-control form-control-lg" 
+                                      required 
+                                      value={data.user.username} 
+                                      onChange={handleChange}
+                                      />
                                     <label className="form-label"  htmlFor="typeUserX">Ususario</label>
                                 </div>
 
                                 <div className="form-outline form-white mb-4">
-                                    <input type="password" id="typePasswordX" className="form-control form-control-lg" />
+                                    <input 
+                                    type="password" 
+                                    name="password" 
+                                    id="typePasswordX" 
+                                    className="form-control form-control-lg" 
+                                    required 
+                                    value={data.user.password}
+                                    onChange={handleChange}
+                                    />
                                     <label className="form-label" htmlFor="typePasswordX">Contraseña</label>
                                 </div>
                             </div>
 
                             <p className="small mb-5 pb-lg-2"><Link className="text-white-50" to="#">Has olvidado tu contraseña?</Link></p>
 
-                            <button className="btn btn-outline-light btn-lg px-5" type="submit">Iniciar sesión</button>
+                            <button className="btn btn-outline-light btn-lg px-5" type="button">Iniciar sesión</button>
 
                             <div>
                               <p className="mb-0">No tienes cuenta? <Link to="/register" className="text-white-50 fw-bold">Registrate</Link></p>
