3217523074f17058bdfd55d699ab32b6d96b8487
Pchaozz <polsh2001.1@gmail.com>
2024-04-11 20:26:30 +0200

    v0.0.5
    - He acabado la base del register i del login, falta la conexion con base de datos

--- magic_market_mobile/lib/login.dart
+++ magic_market_mobile/lib/login.dart
@@ -1,31 +1,108 @@
 import 'package:flutter/material.dart';
-import 'package:magic_market_mobile/dashaboard.dart';
+import 'package:magic_market_mobile/register.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
+  @override
+  _MyLogin createState() => _MyLogin();
+}
 
+class _MyLogin extends State<Login> {
   @override
   Widget build(BuildContext context) {
+    //VARIABLES
+
+    //V - FORM LOGIN
+    final usernameController = TextEditingController();
+    final passwordController = TextEditingController();
+
+    //V - ETC
+    bool _passwordVisible = false;
+
+    //LAYOUT
     return Scaffold(
       appBar: AppBar(
         title: const Text("LOGIN"),
       ),
-      body:  Center (
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            //INPUT USERNAME
+            TextFormField(
+              controller: usernameController,
+              maxLength: 20,
+              decoration: const InputDecoration(
+                  icon: Icon(Icons.account_circle),
+                  labelText: 'Usuari',
+                  labelStyle: TextStyle(color: Color(0xFF6200EE)),
+                  helperText: '',
+                  /*suffixIcon: Icon(
+                  Icons.check_circle,
+                ),*/
+                  enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF6200EE)))),
+            ),
+
+            //INPUT CONTRASENYA
+            TextFormField(
+              keyboardType: TextInputType.text,
+              controller: passwordController,
+              maxLength: 20,
+              obscureText: !_passwordVisible,
+              decoration: const InputDecoration(
+                icon: Icon(Icons.key),
+                labelText: 'Contrasenya',
+                labelStyle: TextStyle(color: Color(0xFF6200EE)),
+                helperText: '',
+                enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFF6200EE))),
+              ),
+            ),
             OutlinedButton(
               onPressed: () {
-              if (1==1) {
-                //AQUI VA LA FUNCION PARA COMPROBAR SI EL LOGIN ES CORRECTO 👍
-              }
+                print("USERNAME: " +
+                    usernameController.text +
+                    " PASSWORD: " +
+                    passwordController.text);
+                _dialogBuilder(context);
               },
               child: const Text('CheckLogin'),
-                         
+            ),
+            OutlinedButton(
+              onPressed: () {
+                print("LOADING REGISTER PAGE");
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Register()),
+                );
+              },
+              child: const Text('Registrarse'),
             )
           ],
         ),
       ),
+    );
+  }
+
+  Future<void> _dialogBuilder(BuildContext context) {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('ERROR'),
+          content: Text('NO SE PUEDE CONECTAR CON LA \n'
+              'BASE DE DATOS..'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Ok'),
+            )
+          ],
+        );
+      },
     );
   }
 }
--- magic_market_mobile/lib/main.dart
+++ magic_market_mobile/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:magic_market_mobile/dashaboard.dart';
+import 'package:magic_market_mobile/register.dart';
 
 import 'login.dart';
 
@@ -11,29 +12,27 @@ void main() {
 const bool isLoged = false;
 
 class MyApp extends StatelessWidget {
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-        title: "MAGIC MARKET ONLINE",
-        routerConfig: _router,
-      );
+      title: "MAGIC MARKET ONLINE",
+      routerConfig: _router,
+    );
   }
-  
+
   final GoRouter _router = GoRouter(
-    
-    redirect: (context, state) {
-      if (isLoged) {
-        return '/dashboard';
-      }
-      return '/login';
-    },
-
-    routes: [
-      GoRoute(path: "/login", builder: (context, state) => const Login()),
-      GoRoute(path: "/dashboard", builder: (context, state) => const Dashboard())
-    ] 
-  );
+      redirect: (context, state) {
+        if (isLoged) {
+          return '/dashboard';
+        }
+        return '/login';
+      },
+      routes: [
+        GoRoute(path: "/login", builder: (context, state) => const Login()),
+        GoRoute(
+            path: "/register", builder: (context, state) => const Register()),
+        GoRoute(
+            path: "/dashboard", builder: (context, state) => const Dashboard())
+      ]);
 }
-
