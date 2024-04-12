02c2dc1c8ce939762bbca8815160d09deead5e5a
raulmartincaro <raul.martincaro@gmail.com>
2024-04-12 17:57:07 +0200

    roles seeder
    creación de seeder de roles.

--- MagicOnlineMarketWeb/database/seeders/RolsSeeder.php
+++ MagicOnlineMarketWeb/database/seeders/RolsSeeder.php
@@ -2,16 +2,39 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class RolsSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+
+        DB::table('rols')->insert([
+            'nom' => 'administrador',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('rols')->insert([
+            'nom' => 'administrador',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('rols')->insert([
+            'nom' => 'venedor',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('rols')->insert([
+            'nom' => 'venedor_premium',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+        DB::table('rols')->insert([
+            'nom' => 'usuari_ban',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
     }
 }
