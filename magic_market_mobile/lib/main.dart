55f96e8af9e0b1327be8bc1ff99b292bad7807dc
Pchaozz <polsh2001.1@gmail.com>
2024-04-17 20:03:44 +0200

    Auth working

--- magic_market_mobile/lib/main.dart
+++ magic_market_mobile/lib/main.dart
@@ -6,42 +6,37 @@ import 'home.dart';
 import 'login.dart';
 
 void main() {
-  runApp(AuthenticationApp());
+  runApp(MyApp());
 }
 
-void initState() {
-  _loadPreferences();
-}
-
-_loadPreferences() async {
-  print("CARGANDO PREFERENCIAS !!!!!!!!!!!!!!");
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  if (prefs.containsKey("Auth")) {
-    isAuthenticated = prefs.getBool("Auth")!;
-  } else {
-    isAuthenticated = false;
-    prefs.setBool("Auth", isAuthenticated);
-  }
-}
-
-class AuthenticationApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Authentication App',
+      title: 'My App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: AuthenticationWrapper(),
+      home: FutureBuilder(
+        future: isAuthenticated(),
+        builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator();
+          } else {
+            if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else {
+              return (snapshot.data ?? false) ? HomePage() : LoginPage();
+            }
+          }
+        },
+      ),
     );
   }
-}
 
-class AuthenticationWrapper extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    print("ESTADO DEL AUTH " + isAuthenticated.toString());
-    return isAuthenticated ? HomePage() : LoginPage();
+  Future<bool> isAuthenticated() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getBool("Auth") ?? false;
   }
 }
