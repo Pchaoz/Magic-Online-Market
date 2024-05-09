d078fbfd0c0ba0e12c71c72b59eff20b5137d3ff
Pchaozz <polsh2001.1@gmail.com>
2024-05-09 19:54:36 +0200

    Update UserController.php
    - Por alguna razon desconocida se habia borrado esta cosa

--- MagicOnlineMarketWeb/app/Http/Controllers/UserController.php
+++ MagicOnlineMarketWeb/app/Http/Controllers/UserController.php
@@ -54,7 +54,8 @@ class UserController extends Controller
     }
 
     public function APIgetUser(Request $request) {
-
+        $user = DB::table('usuaris')->where('nick', $request->query('nickname'))->first();
+        return response()->json($user);
     }
 
 }
