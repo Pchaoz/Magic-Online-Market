7e4ebb36ee95ab7a1d755cc4276d676df9058b4e
Pchaozz <polsh2001.1@gmail.com>
2024-05-09 12:13:36 +0200

    soy subnormal, como que nom = nickaname XDDDDDDDDDDDDDDD

--- magic_market_mobile/lib/Views/profileEditPage.dart
+++ magic_market_mobile/lib/Views/profileEditPage.dart
@@ -14,7 +14,7 @@ class ProfileEditPage extends StatefulWidget {
 }
 
 class _ProfileEditPage extends State<ProfileEditPage> {
-  late Future<List<dynamic>> userInfo;
+  late Future<Map<String, dynamic>> userInfo;
 
   final _formKey = GlobalKey<FormState>();
   String _nick = '';
@@ -29,7 +29,7 @@ class _ProfileEditPage extends State<ProfileEditPage> {
     userInfo = fetchUserInfo();
   }
 
-  Future<List<dynamic>> fetchUserInfo() async {
+  Future<Map<String, dynamic>> fetchUserInfo() async {
     print("USUARIO A CARGAR INFO: " + userName);
     final response =
         await http.get(Uri.parse("$API_URI_SERVER/getUser?nickname=$userName"));
