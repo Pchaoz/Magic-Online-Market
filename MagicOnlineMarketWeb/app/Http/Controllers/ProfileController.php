82b20120cea813da87ea05d5bfa5c0e78a22b350
raulmartincaro <raul.martincaro@gmail.com>
2024-05-20 20:49:06 +0200

    prueba muy terrorista

--- MagicOnlineMarketWeb/app/Http/Controllers/ProfileController.php
+++ MagicOnlineMarketWeb/app/Http/Controllers/ProfileController.php
@@ -24,14 +24,13 @@ class ProfileController extends Controller
     }
 
 
-    public function update(Request $request): RedirectResponse
+    public function update(Request $request)
     {
         $user = $request->user();
         $user->nick = $request->nick;
         $user->name = $request->name;
         $user->cognom = $request->cognom;
         $user->save();
-        return Redirect::route('profile.edit');
     }
 
         /**
