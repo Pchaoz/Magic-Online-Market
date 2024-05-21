7445b91c281d345f6c8189b61acc69e9f2937fc5
Pchaozz <polsh2001.1@gmail.com>
2024-05-21 19:58:13 +0200

    Update ArticleController.php
    ...

--- MagicOnlineMarketWeb/app/Http/Controllers/ArticleController.php
+++ MagicOnlineMarketWeb/app/Http/Controllers/ArticleController.php
@@ -167,6 +167,7 @@ class ArticleController extends Controller
     public function deleteArticleAPI(Request $request) {
 
         $article = Articles::where('idArticle', $request->idArticle)->first();
+        return response()->json([$article], 200);
         $article->delete();
         return response()->json(['message' => 'Artículo eliminado exitosamente'], 200);
     }
