6f04148710d7dae50a655de745a6b130780118ef
Pchaozz <polsh2001.1@gmail.com>
2024-05-27 14:25:59 +0200

    Update balancePage.dart
    trying to make paypal work 💀

--- magic_market_mobile/lib/Views/Profile/balancePage.dart
+++ magic_market_mobile/lib/Views/Profile/balancePage.dart
@@ -59,7 +59,7 @@ class _BalancePageState extends State<BalancePage> {
 
   void _capturePayment(String orderID) async {
     final response = await http.post(
-      Uri.parse('$API_URI_CASA/paypal/capture'),
+      Uri.parse('$API_URI_SERVER/paypal/capture'),
       body: json.encode({'orderID': orderID}),
       headers: {'Content-Type': 'application/json'},
     );
@@ -79,44 +79,44 @@ class _BalancePageState extends State<BalancePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 11, 214, 153),
-          title: const Text('Magic Online Market'),
-        ),
-        body: Column(
-          children: [
-            Container(
-              color: const Color.fromARGB(255, 11, 214, 153),
-              width: double.infinity,
-              child: const Padding(
-                padding: EdgeInsets.all(16),
-                child: Text(
-                  'Salari',
-                  style: TextStyle(fontSize: 24),
-                  textAlign: TextAlign.center,
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 11, 214, 153),
+        title: const Text('Magic Online Market'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            color: const Color.fromARGB(255, 11, 214, 153),
+            width: double.infinity,
+            child: const Padding(
+              padding: EdgeInsets.all(16),
+              child: Text(
+                'Salari',
+                style: TextStyle(fontSize: 24),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Text('Saldo: €$_balance', style: const TextStyle(fontSize: 24)),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: _addBalance,
+                  child: const Text('Recargar Saldo'),
                 ),
-              ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Retirar Saldo'),
+                ),
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  Text('Saldo: €$_balance',
-                      style: const TextStyle(fontSize: 24)),
-                  const SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: _addBalance,
-                    child: const Text('Recargar Saldo'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text('Retirar Saldo'),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
 
@@ -130,17 +130,20 @@ class PayPalWebView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Aprobar Pago'),
+        backgroundColor: const Color.fromARGB(255, 11, 214, 153),
+        title: const Text('Confirmación de Pago'),
       ),
       body: WebView(
         initialUrl: approvalUrl,
         javascriptMode: JavascriptMode.unrestricted,
         navigationDelegate: (NavigationRequest request) {
           if (request.url.contains('return_url')) {
-            String orderID = Uri.parse(request.url).queryParameters['token']!;
-            onApprove(orderID);
-            Navigator.pop(context);
-            return NavigationDecision.prevent;
+            String? orderID = Uri.parse(request.url).queryParameters['token'];
+            if (orderID != null) {
+              onApprove(orderID);
+              Navigator.pop(context);
+              return NavigationDecision.prevent;
+            }
           }
           return NavigationDecision.navigate;
         },
