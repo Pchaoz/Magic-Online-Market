8cab40adbbb270b103f2a0f01d396697bc374db4
Pchaozz <polsh2001.1@gmail.com>
2024-08-19 14:27:36 +0200

    Update Productes.php

--- MagicOnlineMarketWeb/app/Models/Productes.php
+++ MagicOnlineMarketWeb/app/Models/Productes.php
@@ -29,7 +29,7 @@ class Productes extends Model
     }
 
     public function categoriaProducte(){
-        return $this->BelongsTo(CategoriaProductes::class, 'idCategoriesProductes', 'idCategoriesProductes');
+        return $this->BelongsTo(CategoriaProductes::class, 'idCategoriaProductes', 'idCategoriaProductes');
     }
     public function expansioProducte(){
         return $this->BelongsTo(Expansions::class, 'idExpansio', 'idExpansio');
